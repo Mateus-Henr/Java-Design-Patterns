70ebc0e0e121d6b57236404ad3f6f40629b56a49
Mateus-Henr <mateush.study@gmail.com>
2022-05-17 19:28:42 -0300

    Format files

--- Builder/src/implementation/Client.java
+++ Builder/src/implementation/Client.java
@@ -2,7 +2,23 @@ package implementation;
 
 import java.time.LocalDate;
 
-//This is our client which also works as "director"
+/*  Builder Implementation
+
+
+    Implementation steps
+
+    Step 1
+    • We start by creating a builder.
+    • We identify the "parts" of the product and provide methods to create those parts.
+    • It should provide a method to "assemble" or build the product/object.
+    • It must provide a way/method to get fully built object out. Optionally builder can keep reference to a product
+      it has built so the same can be returned again in the future.
+
+    Step 2
+    • A director can be a separate class or client can play the role of director.
+ */
+
+// This is our client which also works as "director"
 public class Client
 {
     public static void main(String[] args)
