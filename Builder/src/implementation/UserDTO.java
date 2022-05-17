e04cd79853e7285f8ee7060427e6bf845bf8a7c8
Mateus Henrique <mateush.study@gmail.com>
2022-05-16 21:51:57 -0300

    Add Builder implementation steps and organize project

---
+++ Builder/src/implementation/UserDTO.java
@@ -0,0 +1,12 @@
+package implementation;
+
+//Interface implemented by "products"
+public interface UserDTO
+{
+    String getName();
+
+    String getAddress();
+
+    String getAge();
+
+}
---
+++ Builder/src/implementation/UserWebDTOBuilder.java
@@ -0,0 +1,8 @@
+package implementation;
+
+// The concrete builder for UserWebDTO.
+// TODO implement builder
+public class UserWebDTOBuilder /*implements UserDTOBuilder */
+{
+
+}
