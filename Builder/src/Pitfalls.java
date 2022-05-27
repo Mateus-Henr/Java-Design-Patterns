354c67f8aec443271ad24815600ef971c866b52b
Mateus Henrique <mateush.study@gmail.com>
2022-05-27 11:31:33 -0300

    Wrap up builder design pattern

---
+++ Builder/src/Pitfalls.java
@@ -0,0 +1,20 @@
+/*  Pitfalls
+
+    • Be aware that if implemented and used correctly there are no pitfalls.
+
+
+    What are the pitfalls when dealing with design patterns
+    • Number of new classes to be added.
+    • Impact to refactor existing code.
+    • Complexity to understand.
+
+
+    Some pitfalls of this pattern
+    • Method chaining (if it's a beginner to Java)
+    • Partially initialized object - Notice that if required properties are missing the Builder has to provide them or
+      throw an exception.
+ */
+
+public class Pitfalls
+{
+}
