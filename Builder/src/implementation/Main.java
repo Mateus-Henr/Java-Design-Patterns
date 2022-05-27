a5953e3cd08eb4173a0fdaa0a7d2ad7af2ec3bdf
Mateus Henrique <mateush.study@gmail.com>
2022-05-27 11:06:04 -0300

    Examples of builder design pattern

--- Builder/src/implementation/Main.java
+++ Builder/src/implementation/Main.java
@@ -1,4 +1,6 @@
-package implementation;/*  Builder Implementation
+package implementation;
+
+/*  Builder Implementation
 
 
     Implementation steps
@@ -12,6 +14,24 @@ package implementation;/*  Builder Implementation
 
     Step 2
     • A director can be a separate class or client can play the role of director.
+
+
+    Examples
+
+    StringBuilder
+    • The java.lang.StringBuilder class as well as various buffer classes in java.nio package like ByteBuffer,
+      CharBuffer are often given as examples of builder pattern.
+    • In my humble opinion they can be given as examples of builder pattern, but with an understanding that they don't
+      match 100% with GoF definition. These classes do allow us to build final object in steps, providing only a part
+      of final object in one step. In this way they can be thought of as an implementation of builder pattern.
+    • So a StringBuilder satisfies the intent/purpose of builder pattern. However, as soon we start looking at structure
+      of the StringBuilder things start to fall apart. GoF definition also states that, builder has potential to build
+      different representations of product interface using same steps. According to the implementation, we could have
+      multiple subclasses that could be built using the same steps.
+
+     CalendarBuilder
+     • Another example of a builder design pattern since it builds part by part of a final calendar product. It also
+       has the "build()" method to assemble everything.
  */
 
 public class Main
