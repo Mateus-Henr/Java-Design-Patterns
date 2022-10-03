395d25b51e60c9b19b8d2473c00d7d0dcde2dcee
mateus <mateush.study@gmail.com>
2022-10-03 15:53:16 -0300

    Add adapter introduction

---
+++ Structural Design Patterns/Adapter/src/ImplementationSteps.java
@@ -0,0 +1,13 @@
+/*
+    Implementation Steps
+
+    • Firstly we create a class for Adapter, and it must implement the interface expected by client.
+    • When using a Class Adapter, we extend from our existing class, and we forward the method to another
+      method inherited from adaptee.
+    • In the case of Object Adapter, we implement the target interface and accept adaptee as constructor
+      argument in adapter by using composition.
+ */
+
+public class ImplementationSteps
+{
+}
