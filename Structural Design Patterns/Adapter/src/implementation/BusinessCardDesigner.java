edb724acea2d934aee73a9c7049ea89cada5ca42
mateus <mateush.study@gmail.com>
2022-10-03 16:33:58 -0300

    Add adapter implementations

---
+++ Structural Design Patterns/Adapter/src/implementation/BusinessCardDesigner.java
@@ -0,0 +1,18 @@
+package implementation;
+
+/**
+ * Client code which requires Customer interface.
+ */
+public class BusinessCardDesigner
+{
+    public String designCard(Customer customer)
+    {
+        String card = "";
+        card += customer.getName();
+        card += "\n" + customer.getDesignation();
+        card += "\n" + customer.getAddress();
+
+        return card;
+    }
+
+}
