6de335dcb505e5b298e77eadb83b1df8e7966d3d
mateus <mateush.study@gmail.com>
2022-10-03 16:30:58 -0300

    Add adapter implementations

--- Structural Design Patterns/Adapter/src/ImplementationSteps.java
+++ Structural Design Patterns/Adapter/src/ImplementationSteps.java
@@ -6,6 +6,9 @@
       method inherited from adaptee.
     • In the case of Object Adapter, we implement the target interface and accept adaptee as constructor
       argument in adapter by using composition.
+    • An object adapter should take adapter as an argument in constructor or a less preferred or a less
+      preferred solution, you can instantiate it in the constructor this tightly coupling with a specific
+      adaptee.
  */
 
 public class ImplementationSteps
