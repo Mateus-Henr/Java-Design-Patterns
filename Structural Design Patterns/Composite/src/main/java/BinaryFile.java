3162af6e832352396a4063ad5c4fedb416ce4583
Matt <mateus.h.figueiredo@ufv.br>
2023-02-22 11:14:58 -0300

    Add Composite design pattern implementation

---
+++ Structural Design Patterns/Composite/src/main/java/BinaryFile.java
@@ -0,0 +1,17 @@
+//Leaf node in composite pattern
+public class BinaryFile extends File
+{
+    private long size;
+
+    public BinaryFile(String name, long size)
+    {
+        super(name);
+        this.size = size;
+    }
+
+    @Override
+    public void ls()
+    {
+        System.out.println(getName() + "\t" + size);
+    }
+}
